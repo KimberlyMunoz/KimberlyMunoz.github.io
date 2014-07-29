9816428284274e48603d96d214a05b4aa98c1c79
Kimberly Munoz <hi@kimberlymunoz.com>
2014-07-28 23:03:25 -0700

    Using Grunt and adding OnePage Scroll
    Getting all fancy

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        concat: {
+            // 2. Configuration for concatinating files goes here.
+            dist: {
+        		src: [
+            		'js/lib/*.js', // All JS in the libs folder
+        		],
+        		dest: 'js/main.js'
+    		}
+        },
+        uglify: {
+    		build: {
+       		 	src: 'js/main.js',
+        		dest: 'js/main.min.js'
+   			}
+		},
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'full-images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'img/'
+                }]
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'sass',
+                    src: ['*.scss'],
+                    dest: 'css/',
+                    ext: '.css'
+                }]
+            } 
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['sass/**/*'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            } 
+
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('default', ['sass']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-kimberlymunoz.com
+#[kimberlymunoz.com](http://www.kimberlymunoz.com)
 =================
 
-Homepage
+This is my homepage. Right now it's just static HTML and CSS.
+
+## Made With
+[One Page Scroll v1.3](https://github.com/peachananr/onepage-scroll)
